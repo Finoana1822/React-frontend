8c09111769c5fa65436857ff1d9ae4c6ca0f898b
Mario Lovamanitra <lovamanitra.randriamanantena@esti.mg>
2022-10-04 11:40:04 +0300

    modified database

--- src/Pages/Formation.jsx
+++ src/Pages/Formation.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Formation = () => {
+    const link = 'http://127.0.0.1:8000/api/formations/';
+    const [Formation, setFormation] = useState();
+    useEffect(()=>{
+        getFormation();
+    },[])
+    const getFormation = async () => {
+        await axios.get()
+    }
     return (
         <div>
             
